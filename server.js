4edefe4d305eda3f8e5cc16a0036fdc9515b30ed
adityak14 <aditya.kaushik1213@gmail.com>
2022-07-28 00:03:28 +0530

    Adding items to the cart

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
@@ -5,6 +6,10 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbstore = require('connect-mongo')
+// MongoDbstore(session)
 
 // Database connection
 const url="mongodb+srv://aditya:aditya123@cluster0.s8e6v.mongodb.net/pizza"
@@ -24,10 +29,34 @@ try {
   connection.on("error", (err) => console.log(`Connection error ${err}`));
   connection.once("open", () => console.log("Connected to DB!"));
 
+// Session store
+let mongoStore = MongoDbstore.create({
+  mongooseConnection: connection,
+  collection: 'sessions',
+  mongoUrl: url
+})
 
+//Session config
+app.use(session({
+  secret:process.env.COOKIE_SECRET,
+  resave: false,
+  store: mongoStore,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+}))
+
+app.use(flash())
 
 //assets
 app.use(express.static('public'))
+app.use(express.json())
+
+// Global middleware
+app.use((req, res, next) => {
+  res.locals.session = req.session
+  // res.locals.user = req.user
+  next()
+})
 
 //set Template engine
 app.use(expressLayout)
